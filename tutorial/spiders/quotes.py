80c7a74e6e0ccb239bdffb1326ee0bb5bb23f098
kenneth irungu <kennethirungu1800@gmail.com>
2019-09-23 08:56:02 -0400

    made my first web scrawling on the readme website

--- tutorial/spiders/quotes.py
+++ tutorial/spiders/quotes.py
@@ -1,20 +1,17 @@
 import scrapy
 
-
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
     def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        url ='https://www.goodreads.com/quotes'
+        
+        yield scrapy.Request(url=url , callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        for quote in response.selector.xpath("//div[@class ='quote']"):
+            yield{
+                'text': quote.xpath(".//div[@class='quoteText']/text()[1]").extract_first(),
+                'author':quote.xpath(".//div[@class='quoteText']/child::span").extract_first(),
+                'tags':quote.xpath(".//div[@class='greyText smallText left']/a/text()").extract_first(),
+            }
